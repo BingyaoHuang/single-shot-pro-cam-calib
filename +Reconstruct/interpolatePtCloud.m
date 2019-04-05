650e47c06a3f748abb920c78320fa5cb02fd945d
Bingyao Huang <hby001@gmail.com>
2019-04-05 13:58:31 -0400

    Mod: use bilinear interpolation for point cloud and mesh texture

--- +Reconstruct/interpolatePtCloud.m
+++ +Reconstruct/interpolatePtCloud.m
@@ -49,34 +49,57 @@ pts3d = [xq(:), yq(:), zq(:)];
 %% only keep the interpolated points in mask ROI
 % project interpolated mesh points to camera image
 pts2d = cv.projectPoints(pts3d, zeros(3,1), zeros(3,1), camK, 'DistCoeffs', camKc);
-
-% convert to linear indices
-pts2d = round(pts2d);
 pts2dColor = pts2d;
 
-% filter point cloud
+% remove points that are out of camera's fov
 inlierIdx = (pts2d(:,2) > 0) & (pts2d(:,1) > 0) & (pts2d(:,2) < size(imMask,1)) & (pts2d(:,1) < size(imMask,2));
+pts2dColor(~inlierIdx,:) = 1;
 pts2d = pts2d(inlierIdx,:);
 pts3d = pts3d(inlierIdx,:);
-pts2dIdx = sub2ind(size(imMask), pts2d(:,2), pts2d(:,1));
 
-% keep 2d, 3d point in ROI
-pixVal = imMask(pts2dIdx);
+% only keep masked 2d, 3d points
+pixVal = interp2(im2single(imMask), pts2d(:, 1), pts2d(:, 2));
 pts2dInliers = pts2d(pixVal>0,:);
 pts3dInliers = pts3d(pixVal>0,:);
 
-if(1)
-    % plot surf
-    pts2dColor(pts2dColor <= 0) = 1;
-    pts2dColor(pts2dColor(:,1) > size(imMask,2), 1) = size(imMask,2);
-    pts2dColor(pts2dColor(:,2) > size(imMask,1), 2) = size(imMask,1);
-    
-    r = reshape(imROI(sub2ind(size(imROI), pts2dColor(:,2), pts2dColor(:, 1), 1*ones(size(pts2dColor,1), 1))), [numPts+1, numPts+1]);
-    g = reshape(imROI(sub2ind(size(imROI), pts2dColor(:,2), pts2dColor(:, 1), 2*ones(size(pts2dColor,1), 1))), [numPts+1, numPts+1]);
-    b = reshape(imROI(sub2ind(size(imROI), pts2dColor(:,2), pts2dColor(:, 1), 3*ones(size(pts2dColor,1), 1))), [numPts+1, numPts+1]);
-    
+% convert to single for color interpolation
+imROI = im2single(imROI);
+
+% assign color to pt3d
+r = interp2(imROI(:,:,1), pts2dInliers(:, 1), pts2dInliers(:, 2));
+g = interp2(imROI(:,:,2), pts2dInliers(:, 1), pts2dInliers(:, 2));
+b = interp2(imROI(:,:,3), pts2dInliers(:, 1), pts2dInliers(:, 2));
+
+% create ptCloud with colors
+ptCloudOut = pointCloud(pts3dInliers, 'Color', [r,g,b]);
+
+%% Debug info
+if(verbose)
+    %% plot point cloud
     figure;
-    h = surf(xq, yq, zq, cat(3, r, g, b), 'FaceColor', 'texturemap', 'EdgeColor', 'texturemap', 'FaceLighting', 'gouraud');
+    pcshow(ptCloudOut, 'VerticalAxis', 'y', 'VerticalAxisDir', 'down', 'MarkerSize', 60);
+    title('Interpolated point cloud with color')
+    daspect([1 1 1]);
+    % view(3);
+    axis vis3d tight;
+    
+    %% plot mesh (edges only)
+    figure;
+    scatter3(x,y,z, 'bo'); hold on
+    mesh(xq, yq, zq);hold off
+    title('Interpolated mesh, Nodes are shown as blue circles');
+    daspect([1 1 1]);
+    % view(3);
+    axis vis3d tight;
+    
+    %% plot surface
+    % interpolate color
+    r = reshape(interp2(imROI(:,:,1), pts2dColor(:, 1), pts2dColor(:, 2)), [numPts+1, numPts+1]);
+    g = reshape(interp2(imROI(:,:,2), pts2dColor(:, 1), pts2dColor(:, 2)), [numPts+1, numPts+1]);
+    b = reshape(interp2(imROI(:,:,3), pts2dColor(:, 1), pts2dColor(:, 2)), [numPts+1, numPts+1]);
+
+    figure;
+    h = surf(xq, yq, zq, cat(3, r, g, b), 'FaceColor', 'texturemap', 'EdgeColor', 'texturemap', 'FaceLighting', 'gouraud', 'LineStyle', 'none');
     title('Reconstructed mesh')
     h.XData(pixVal==0) = nan;
     h.YData(pixVal==0) = nan;
@@ -87,42 +110,5 @@ if(1)
     % view(3);
     axis vis3d tight;
     rotate3d on
-end
-
-% convert ROI points 2d to linear indices
-pts2dInlierIdx = sub2ind(size(imMask), pts2dInliers(:,2), pts2dInliers(:,1));
-
-% assign color to pt3d
-pts3dInliersColor = [];
-for i=1:3
-    imC = imROI(:,:,i);
-    
-    % concatenate RGB channel to pt3dColor
-    pts3dInliersColor = [pts3dInliersColor, imC(pts2dInlierIdx)];
-end
-
-% create ptCloud with colors
-ptCloudOut = pointCloud(pts3dInliers, 'Color', pts3dInliersColor);
-
-%% Debug info
-if(verbose)
-    figure;
-    pcshow(ptCloudOut, 'VerticalAxis', 'y', 'VerticalAxisDir', 'down', 'MarkerSize', 60);
-    title('Interpolated point cloud with color')
-    daspect([1 1 1]);
-    % view(3);
-    axis vis3d tight;
-    
-    figure;
-    scatter3(x,y,z, 'bo');
-    hold on
-    % scatter3(ROIpt3d(:,1),ROIpt3d(:,2),ROIpt3d(:,3),'go');hold off
-    mesh(xq, yq, zq);hold off
-    title('Interpolated mesh with Nodes in blue circle');
-    
-    % plot3(x,y,z,'o');
-    daspect([1 1 1]);
-    % view(3);
-    axis vis3d tight;
 end
 end
