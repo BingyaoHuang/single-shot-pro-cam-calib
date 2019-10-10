bbd05834be1c2e8e8d84fc862a30787d3aca4c4e
Bingyao Huang <hby001@gmail.com>
2019-10-09 20:14:35 -0400

    Fix: a bug that alignment error is miscalculated and reconstruction looks ugly

--- +Reconstruct/alignmentError.m
+++ +Reconstruct/alignmentError.m
@@ -30,18 +30,63 @@ if(nargin < 7)
     figName = '';
 end
 
-pts3d = pcCalib.Location;
+%%
+% convert point cloud to XYZ map
+% imPC = zeros(camH, camW, 3);
+% imPC(:,:,1) = (reshape(pcGroundTruth.Location(:,1), camW, camH))';
+% imPC(:,:,2) = (reshape(pcGroundTruth.Location(:,2), camW, camH))';
+% imPC(:,:,3) = (reshape(pcGroundTruth.Location(:,3), camW, camH))';
+% imPC = double(imPC);
 
-% convert point cloud to depth map
-imD = (reshape(pcGroundTruth.Location(:,3), camW, camH))';
-imD = double(imD);
+% pts3dGT = double(pcGroundTruth.Location);
+% pts3dGT = pts3dGT(pts3dGT(:,3)>0,:);
+% pts2dGT = cv.projectPoints(pts3dGT, [0,0,0], [0,0,0], param.camK, 'DistCoeffs', param.camKc);
+% imD = zeros(camH, camW);
+% pts2dGT = round(pts2dGT);
+% inlierIdx = pts2dGT(:,1)<=camW & pts2dGT(:,2)<=camH & pts2dGT(:,1)>0 & pts2dGT(:,2)>0;
+% pts2dGT = pts2dGT(inlierIdx,:);
+% pts3dGT = pts3dGT(inlierIdx,:);
+% imD(sub2ind([camH, camW], pts2dGT(:,2), pts2dGT(:,1))) = pts3dGT(:,3);
+
+pts3d = double(pcCalib.Location);
+pts3d = pts3d(pts3d(:,3)>0,:);
+pts2d = cv.projectPoints(pts3d, [0,0,0], [0,0,0], param.camK, 'DistCoeffs', param.camKc);
+imD = zeros(camH, camW);
+pts2d = round(pts2d);
+inlierIdx = pts2d(:,1)<=camW & pts2d(:,2)<=camH & pts2d(:,1)>0 & pts2d(:,2)>0;
+pts2d = pts2d(inlierIdx,:);
+pts3d = pts3d(inlierIdx,:);
+imD(sub2ind([camH, camW], pts2d(:,2), pts2d(:,1))) = pts3d(:,3);
+
+% find depth image mask
+imMask = false(size(imD));
+imMask(imD > 0) = 1;
+se = strel('disk', 5);
+% imMask = imclose(imopen(imMask,  se), se);
+imMask = imclose(imMask, se);
+imMask = imfill(imMask, 'holes');
+imMask = bwareafilt(imMask, 1);
+imMask = imgaussfilt(double(imMask), 7) > 0.2; % smooth edges
+% fs(imMask);
+
+%% align reconstructed point cloud with ground truth point cloud using icp
+
+% downsample and denoise to save time and memory
+% gridSize = 0.1;
+% fixed = pcdownsample(pcdenoise(pcGroundTruth, 'Threshold',5), 'gridAverage', gridSize);
+% % moving = pcdenoise(pcdownsample(pointCloud(pts3dInterp), 'gridAverage', gridSize));
+% moving = pcdownsample(pcdenoise(pcCalib,'Threshold',5), 'gridAverage', gridSize);
+% 
+% % register pcCalib to fixed pcGroundTruth
+% [tform, ~, ~] = pcregistericp(moving, fixed,'Extrapolate', true, 'MaxIterations', 50);
+% pcCalibAligned = pctransform(pcCalib, tform);
+% figure;pcshowpair(fixed, pcCalibAligned)
 
 %% interpolate point cloud
-imMask = zeros(size(imD));
-imMask(imD > 0) = 1;
 rp = regionprops(imMask, 'boundingbox');
-npX = rp.BoundingBox(3) / 2;
-npY = rp.BoundingBox(4) / 2;
+desity = 1;
+npX = rp.BoundingBox(3)*desity;
+npY = rp.BoundingBox(4)*desity;
 
 % xRange contains lower and higher bounds
 xRange = double(pcCalib.XLimits);
@@ -53,20 +98,25 @@ yDiff = yRange(1) - yRange(2);
 yStep = -yDiff/npY;
 
 % use griddata to interpolate a mesh from given point cloud
+% pts3d = pcCalibAligned.Location;
 [xq,yq] = meshgrid(xRange(1):xStep:xRange(2), yRange(1):yStep:yRange(2));
 zq = griddata(pts3d(:,1),pts3d(:,2),pts3d(:,3), xq, yq,'cubic');
 
-% pts3dInterp is the interpolated denser point cloud
-pts3dInterp = [xq(:), yq(:), zq(:)];
+% pcInterp is the interpolated denser point cloud
+pcInterp = pointCloud([xq(:), yq(:), zq(:)]);
 
-%% crop extra interpolated mesh using ground truth point cloud
-[~, pts3dInterpAligned, ~] = pcregrigid(pointCloud(pts3dInterp), pcGroundTruth,'Extrapolate', true);
-[~, ~, ~, pcInlierIdx] = Reconstruct.filterPointCloud(pts3dInterpAligned, imD, param, camW, camH);
+% finer registration
+% moving = pcdownsample(pcdenoise(pcInterpAligned,'Threshold',5), 'gridAverage', gridSize);
+% [tform, ~, ~] = pcregrigid(moving, fixed,'Extrapolate', true);
+% pcInterpAligned = pctransform(pcInterpAligned, tform);
+
+[~, ~, ~, pcInlierIdx] = Reconstruct.filterPointCloud(pcInterp, imMask, param, camW, camH);
 pcOutlierIdx = setdiff(1:size(xq,1)*size(xq,2), pcInlierIdx);
 
 if(verbose)
     figure('Name', figName);
-    h = surf(xq, yq, zq, 'FaceColor', 'interp', 'FaceLighting', 'gouraud');
+    h = surf(xq, yq, zq, 'FaceColor', 'interp', 'FaceLighting', 'gouraud'); % square mesh
+%     h = trisurf(delaunay(xq,yq),xq, yq, zq, 'FaceColor', 'interp', 'FaceLighting', 'gouraud'); % triangular mesh
     
     % remove interpolated meth that are out of ground truth range
     h.XData(pcOutlierIdx) = nan;
@@ -74,15 +124,15 @@ if(verbose)
     h.ZData(pcOutlierIdx) = nan;
     h.CData(pcOutlierIdx) = nan;
     
-%     h.EdgeColor = 'none';
-    h.EdgeColor = [0.3,0.3,0.3];
+    h.EdgeColor = 'none';
+%     h.EdgeColor = [0.3,0.3,0.3];
     h.LineStyle = ':';
-    colormap jet
+    colormap parula
     
     % title
     title(figName)
-    caxis([-50 50])
-    colorbar
+    caxis([0 50])
+%     colorbar
     
     % remove ticks
     set(gca,'xtick',[])
@@ -98,19 +148,23 @@ if(verbose)
     axis vis3d tight
     
     rotate3d on
+    box on
+    set(gca, 'visible', 'off'); % ge trid of ugly box edges
 end
 
-% light('Position',[0 1 -1]) 
+light('Position',[0 1 -1]);
+material dull % metal, shiny, default
 
 %% calculte 3D alignment error using knn search
-[~, err] = knnsearch(pcGroundTruth.Location, pts3dInterp);
+[~, err] = knnsearch(pcGroundTruth.Location, pcInterp.Location);
 
 % visualize errors as pseudocolor
 if(verbose)
     errOutlierIdx = isnan(err);
     errInlierIdx = ~errOutlierIdx;
     h.ZData(errOutlierIdx) = nan;
-    h.CData(errInlierIdx) = err(errInlierIdx); % use err as color      
+    h.CData(errInlierIdx) = err(errInlierIdx); % use err as color  
+%      h.CData(errInlierIdx) = abs(err(errInlierIdx)); % use abs err as color 
 end
 
 % remove nan values
