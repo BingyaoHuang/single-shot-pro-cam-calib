59b000f3d4d3c1f3d4c12d3dc351071f06e5abe2
Bingyao Huang <hby001@gmail.com>
2020-03-19 21:16:06 -0400

    Add: comments and license

--- +ImgProc/findNbInDir.m
+++ +ImgProc/findNbInDir.m
@@ -1,7 +1,29 @@
+function nextId = findNbInDir(Nodes, id, direction, iIn)
 %% Find all nodes along a given direction in the graph
 % this function returns a list of Nodes id on the same stripe,
 % the search direction is specified by user input
-function nextId = findNbInDir(Nodes, id, direction, iIn)
+
+%% License
+% ACADEMIC OR NON-PROFIT ORGANIZATION NONCOMMERCIAL RESEARCH USE ONLY
+% Copyright (c) 2018 Bingyao Huang
+% All rights reserved.
+
+% Redistribution and use in source and binary forms, with or without
+% modification, are permitted provided that the following conditions are met:
+
+% The above copyright notice and this permission notice shall be included in all
+% copies or substantial portions of the Software.
+% 
+% If you publish results obtained using this software, please cite our paper.
+
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+% SOFTWARE.
+
 % Enable for debugging..
 %     persistent i;
 %
