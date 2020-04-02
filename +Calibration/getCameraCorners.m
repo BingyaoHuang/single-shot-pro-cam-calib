267398a7f51cfddc49e9d74747ea3f2f50848e3b
Bingyao Huang <hby001@gmail.com>
2020-04-02 15:13:29 -0400

    Fix a bug that readAllImgs is not defined

--- +Calibration/getCameraCorners.m
+++ +Calibration/getCameraCorners.m
@@ -60,4 +60,30 @@ if(nnz(usedImIdx) < length(usedImIdx))
 end
 end
 
+function imD = readAllImgs(imgPath, ext, idx)
+
+if(nargin < 2)
+    imgDs = imageDatastore(imgPath);
+else
+    imgDs = imageDatastore(imgPath, 'FileExtensions', ext);
+end
+
+numImgs = length(imgDs.Files);
+
+if(nargin < 3)
+    idx = 1:numImgs;
+end
+
+% read all images as cell (very fast)
+imD = imgDs.readall;
+
+% keep only the idx
+imD = imD(idx);
+
+% convert to 4D mat
+imD = cat(4, imD{:});
+end
+
+
+
 
